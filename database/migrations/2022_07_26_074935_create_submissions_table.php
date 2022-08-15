68121a7b1cabd4282ae57fe710c49a4a48a048a5
Hussains3 <shussain8978@gmail.com>
2022-08-15 18:32:14 +0600

    Database design

--- database/migrations/2022_07_26_074935_create_submissions_table.php
+++ database/migrations/2022_07_26_074935_create_submissions_table.php
@@ -15,6 +15,19 @@ return new class extends Migration
     {
         Schema::create('submissions', function (Blueprint $table) {
             $table->id();
+            $table->string('submission_id')->nullable();
+            $table->unsignedBigInteger('type_id')->nullable();
+            $table->foreign('type_id')->references('id')->on('article_types');
+            $table->string('cover_letter')->nullable();
+            $table->string('ms_file')->nullable();
+            $table->string('akmnt')->nullable();
+            $table->string('coi')->nullable();
+            $table->longText('comment')->nullable();
+            $table->longText('full_title')->nullable();
+            $table->longText('short_title')->nullable();
+            $table->longText('abstract')->nullable();
+            $table->longText('keywords')->nullable();
+            $table->longText('funding')->nullable();
             $table->timestamps();
         });
     }
